3e5200fc430494afcd318fd6eaa8e2787dcc5033
Apivato <apivato21@gmail.com>
2022-05-16 19:13:23 +0000

    agent scripts tested succesfully

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,32 +1,36 @@
 #! /bin/bash
 
+#command line arguments 
 psql_host=$1
 psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
 
-if [ $# -ne 5]; then
+#check if there are the right amount of arguments
+if [ $# -ne 5 ]; then
   echo 'Insuficient number of arguments for given command'
   exit 1
 fi
 
+#set up the varibles for the insert and parse the host machines specs
 lscpu_out=`lscpu`
 hostname=$(hostname -f)
 
 timestamp=$(vmstat -t | awk 'FNR == 3 {print $18, $19}')
-
-#Retrieve hardware specification 
+ 
 cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out" | egrep "^Architecture:" | awk '{print $2}' | xargs)
 cpu_model=$(echo "$lscpu_out" | egrep "^Model name:" | awk '{$1=$2=$3=""; print $0}' | xargs)
 cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU\sMHz:" | awk '{print $3}' | xargs)
-L2_cache=$(echo "$lscpu_out" | egrep "^L2\scache:" | awk '{print $3}' | xargs)
+L2_cache=$(echo "$lscpu_out" | egrep "^L2\scache:" | awk '{print $3}' | grep -o -E '[0-9]+' | xargs)
 total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 
-insert_stmt="INSERT INTO host_usage (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, timestamp) VALUES($hostname, $cpu_number, $cpu_architecture, $cput_model, $cpu_mhz, $L2_cache, $total_mem, $timestamp);"
+#sql insert statement to be used when connecting to psql database and store host machine specs
+insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, timestamp) VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$L2_cache', '$total_mem', '$timestamp');"
 
 export PGPASSWORD=$psql_password 
 
+#psql command to connect and run insert statment
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 exit $?
--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 
+#command line arguments
 psql_host=$1
 psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
 
-
-if [ $# -ne 5]; then
+#check if there are the appropriate amount of commandline argumetns
+if [ $# -ne 5 ]; then
   echo 'invalide number of arguments for given command'
   exit 1
 fi
 
+#set up host machine usage variables and parsing them
 vmstat_mb=$(vmstat --unit M)
 hostname=$(hostname -f)
 
@@ -23,11 +25,14 @@ cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -n1 | xargs)
 disk_io=$(vmstat -d | awk 'FNR == 3 {print $10}')
 disk_available=$(df -m / | awk 'FNR == 2 {print $4}')
 
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+#subquery to obtain host id from the host_info table
+host_id="(SELECT id FROM host_info WHERE hostname = '$hostname')";
 
-insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES($timestamp, $host_id, $memory_free, $cpu_idle, $cpu_kernel, $disk_io, $disk_available);"
+#insert query to store host usage parsed data into database
+insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES('$timestamp', $host_id, '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available');"
 
 export PGPASSWORD=$psql_password 
 
+#psql command to connect and run insert query and send data into the database
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 exit $?
