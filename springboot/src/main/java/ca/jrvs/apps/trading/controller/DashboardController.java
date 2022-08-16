2e1bb26ed2cc52e3a77a8fc74207be954d5611c0
Apivato <apivato21@gmail.com>
2022-08-16 16:40:53 -0400

    update

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/DashboardController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/DashboardController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/dashboard")
-@CrossOrigin(origins = "https://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class DashboardController {
 
     private DashboardService dashboardService;
--- springboot/src/main/java/ca/jrvs/apps/trading/controller/TraderAccountController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/TraderAccountController.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/trader")
-@CrossOrigin(origins = "https://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class TraderAccountController {
 
     private TraderAccountService traderAccountService;
